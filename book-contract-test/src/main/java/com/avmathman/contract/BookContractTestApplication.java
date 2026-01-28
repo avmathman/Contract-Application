8b2dd5e0122f091c3d4d58ee5b3bb39024145b42
Abdurashid Vakhobov <avcoding01@gmail.com>
2026-01-28 18:15:21 +0500

    initial commit

---
+++ book-contract-test/src/main/java/com/avmathman/contract/BookContractTestApplication.java
@@ -0,0 +1,13 @@
+package com.avmathman.contract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookContractTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookContractTestApplication.class, args);
+    }
+
+}
---
+++ book-contract-test/src/test/java/com/avmathman/contract/BookContractTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avmathman.contract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookContractTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
